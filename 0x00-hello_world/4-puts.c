5b787ba632189f4c8dd428111b95bdd1c2e7fa8f
dfizzkruah93 <dfizzkruah@gmail.com>
2022-10-13 15:20:42 -0700

    create  4-puts file and edit  4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
-#include <studio.h>
+#include <stdio.h>
+
+
 /**
- * main - Entry
- * Return: Always 0
+ * main - Entry Point
+ *
+ * Return: Always 0 (Succcess)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
