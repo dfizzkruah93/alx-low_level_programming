8592c9aa2db665b679ea23aeb8ec0904a2f0905c
dfizzkruah93 <dfizzkruah@gmail.com>
2022-10-19 06:16:09 -0700

    fix error

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,8 +6,9 @@
  */
 int _isalpha(int c)
 {
-if ((c >= 'a' && c <= 'z') || (c >= 'A' && >= 'Z'))
-	return (1);
-else
-	return (0);
+
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && >= 'Z'))
+		return (1);
+	else
+		return (0);
 }
