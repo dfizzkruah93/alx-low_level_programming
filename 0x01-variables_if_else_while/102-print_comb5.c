834efd3c8d96acbfde9092341275d51caa0205ee
dfizzkruah93 <dfizzkruah@gmail.com>
2023-12-04 05:29:59 -0800

    fix error

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,17 +11,17 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0; i < 99; i++)
+	for (i = 0; i < 100; i++)
 	{
-		for (j = 0; j < 99; j++)
+		for (j = 0; j < 100; j++)
 		{
 			if (i < j)
 			{
-				putchar((i / 10) + '0');
-				putchar((i % 10) + '0');
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
 				putchar(' ');
-				putchar((j / 10) + '0');
-				putchar((j % 10) + '0');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
 
 				if (i != 99 || j != 99)
 				{
