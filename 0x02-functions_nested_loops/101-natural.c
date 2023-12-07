f1a0616cee1bb94b4c31102de073ab9ab6117bcb
dfizzkruah93 <dfizzkruah@gmail.com>
2023-12-07 07:16:57 -0800

    try fixing error

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -13,10 +13,10 @@ int main(void)
 
 	for (i = 0; i < 1024; i++)
 	{
-		(if i % 3 == 0 || i % 5 == 0)
-
-			sum += i;
+		if (i % 3 == 0 || i % 5 == 0)
+			sum = sum + i;
 	}
 	printf("%d", sum);
+
 	return (0);
 }
