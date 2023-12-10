009702fcb20c753f2a76aec41d3eee5945968472
dfizzkruah93 <dfizzkruah@gmail.com>
2023-12-10 09:08:58 -0800

    create file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line -  function that draw a straight line in the terminal
+ *
+ *@n: number of time time line is printed
+ *Return: void
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i;
+
+		for (i = 1; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
