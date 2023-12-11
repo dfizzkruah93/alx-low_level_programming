62808d536cde24f2189a1259db17370fbb815dda
dfizzkruah93 <dfizzkruah@gmail.com>
2023-12-11 06:29:15 -0800

    create task 1

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
-*  reset_to_98 - takes the pointer and updates the value
-*  @n: int to check
-*  *  Return: 0 is success
-*/
+ * reset_to_98 - reset n to 98
+ *
+ *@n: number to reset
+ *Return: void
+ */
 
 void reset_to_98(int *n)
 {
