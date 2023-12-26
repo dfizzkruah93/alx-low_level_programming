cb50ef13ae1628c23252f89a52c7cdcc28a5fec9
dfizzkruah93 <dfizzkruah@gmail.com>
2023-12-26 00:58:59 -0800

    fix error

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,7 +3,7 @@
 /**
  * puts_half - function that prints half of a string, followed by a new line
  *
- * @str: string to be printed
+ *@str: string to be printed
  *Return: string
  */
 
@@ -12,7 +12,7 @@ void puts_half(char *str)
 	int i, n;
 	int count = 0;
 
-	for (i = 0; str[1] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 		count++;
 	n = (count - 1) / 2;
 	for (i = n + 1; str[i] != '\0'; i++)
