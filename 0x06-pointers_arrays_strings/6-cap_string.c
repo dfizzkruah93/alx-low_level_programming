8a5e2fbf9e2a6cfab7eded497d5c665c249a7f6b
dfizzkruah93 <dfizzkruah@gmail.com>
2024-01-05 06:56:56 -0800

    fix error

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,7 +14,7 @@ char *cap_string(char *str)
 	char sep[13] = {' ', '\t', '\n', ',', ';', '.',
 '!', '?', '"', '(', ')', '{', '}'};
 
-	for (i = 0; str[i] != '0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
 		{
