2f8569568f40c9ac7094948003d98525ccf16dbe
dfizzkruah93 <dfizzkruah@gmail.com>
2024-01-05 06:51:24 -0800

    create file

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ *
+ * @str: string to be capitilized
+ * Return: pointer to the string
+ */
+
+char *cap_string(char *str)
+{
+	int i, j;
+
+	char sep[13] = {' ', '\t', '\n', ',', ';', '.',
+'!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; str[i] != '0'; i++)
+	{
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		for (j = 0; j < 13; j++)
+		{
+			if (str[i] == sep[j])
+			{
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				{
+					str[i + 1] = str[i + 1] - 32;
+				}
+			}
+		}
+	}
+	return (str);
+}
+
