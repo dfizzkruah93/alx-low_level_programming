734cb3e0a0eaa372ed9c17ddef9c93c3abd1a42f
dfizzkruah93 <dfizzkruah@gmail.com>
2024-01-08 02:50:28 -0800

    create file

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - function that copies memory area
+ *
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+
+		dest[i] = src[i];
+	return (dest);
+}
