2a1df921a54e2363453b8ee712b945f0cb0d9c3b
dfizzkruah93 <dfizzkruah@gmail.com>
2024-01-08 05:23:55 -0800

    fix error

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- *  _strspn - function that gets the length of a prefix substring
+ * _strspn - function that gets the length of a prefix substring
  *
- * @s:  number of bytes of initial string
- * @accept: substring
- * Return: length of bytes
+ * @s: string bytes
+ * @accept: string prefix
+ * Return: length of prefix
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
+
 	int i, j;
 
-	for (i = 0; s[i] != '\n'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; s[i] != accept[j]; j++)
 
