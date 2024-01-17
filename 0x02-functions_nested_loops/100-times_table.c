e8ea65fd7760cfee6cc96720d64a91119c1ee6d6
dfizzkruah93 <dfizzkruah@gmail.com>
2024-01-17 04:14:46 -0800

    fix error

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,29 +6,38 @@
  */
 void print_times_table(int n)
 {
-    int i;
-    int j;
 
-    if ( i>= 0 && i <= 15)
-    {
- 	    for (i = 0; i <= n; i++)
-            {
-		    for (j = 0; j <= n; j++)
-			    int result = i * j;
-		    if (j != 0)
-	            {
-			    _putchar(',');
-			    _putchar(' ');
-			    
-			    if (result < 10)
-				    _putchar(' ');
-			    if (result < 100)
-				    _putchar(' ');
-		    }
-		    _putchar(result / 100 + '0');
-		    _putchar((result / 10) % 10 + '0');
-		    _putchar(result % 10 + '0');
-	    }
-	    _putchar('\n');
-    }
+	int row, col, product;
+
+	if (n < 0 || n > 15)
+	{
+		return;
+	}
+
+	for (row = 0; row <= n; row++)
+	{
+		for (col = 0; col <= n; col++)
+		{
+			product = row * col;
+
+		}
+		if (col != 0)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		if (product < 10)
+		{
+			_putchar(' ');
+			_putchar(' ');
+		}
+		else if (product < 100)
+		{
+			_putchar(' ');
+		}
+		_putchar(product / 100 + '0');
+		_putchar(product / 10 % 10 + '0');
+		_putchar(product % 10 + '0');
+	}
+	_putchar('\n');
 }
