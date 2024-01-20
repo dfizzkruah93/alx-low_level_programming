beeb539ed3d278f723fbabe0ac47cf89fc0f3b69
dfizzkruah93 <dfizzkruah@gmail.com>
2024-01-20 06:55:35 -0800

    create file

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,39 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <time.h>
 
 /**
- * main - program that generates random valid passwords
+ * checksum - check the sum of the string
+ *
+ * @s: strint to checked
+ *Return: sum of the string
+ */
+
+unsigned long int checksum(char *s)
+{
+	unsigned long int sum = 0;
+
+	for (; *s != 0; s++)
+	{
+		sum += *s;
+	}
+	return (sum);
+}
+
+/**
+ * main - print password for crackme
  *
  * Return: 0
  */
 
-#define PASSWORD_LENGTH 6
-
-char generateRandomChar()
+int main(void)
 {
-	return 'A' + (rand() % 26);
-}
+	unsigned long int sum;
+	char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFHHIJKLMNOPQRSTUVWXYZ";
+	char s[33];
+	int i, flag = 0;
 
-void generateRandomPassword(char *password)
-{
-	int i;
-
-	for (i = 0; i < PASSWORD_LENGTH; i++)
+	while (flag == 0)
 	{
-		password[i] = generateRandomChar();
+		for (i = 0; i < 33; i++)
+		{
+			s[i] = alpha[rand() % sizeof(alpha) - 1];
+		}
+		s[i] = '\0';
+		sum = checksum(s);
+
+		if (sum == 2772)
+		{
+			flag = 1;
+			printf("%s\n", s);
+		}
 	}
-	password[PASSWORD_LENGTH] = '\0';
-
-}
-
-int main()
-{
-	srand(time(NULL));
-
-	char password[PASSWORD_LENGTH + 1];
-	generateRandomPassword(password);
-	printf("Generated password:%s\n", password);
-
 	return (0);
 }
