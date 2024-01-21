348d19c952771a2fd0f76d9794fe08c7f16a3d90
dfizzkruah93 <dfizzkruah@gmail.com>
2024-01-21 04:12:48 -0800

    create file 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints its name, followed by a new line
+ *
+ * @argc: count argument
+ * @argv: store argument
+ * Return: 0
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
