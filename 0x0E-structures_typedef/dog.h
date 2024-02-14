96bbd2082c6769196f8554a692c3f3fe9007d21c
dfizzkruah93 <dfizzkruah@gmail.com>
2024-02-14 01:42:44 -0800

    create file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * free dog - function that free dog
+ *
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dof
+ */
+
+struct dog
+{
+
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
