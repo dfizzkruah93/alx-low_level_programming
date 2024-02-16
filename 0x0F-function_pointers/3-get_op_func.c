cbc91410df69a2bced8bdc46ccfad7c807d39372
dfizzkruah93 <dfizzkruah@gmail.com>
2024-02-16 09:15:24 -0800

    fix error

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -12,16 +12,15 @@
 
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[i] = {
-	{ "+", op_add };
-	{ "-", op_sub };
-	{ "*", op_mul };
-	{ "/", op_div };
-	{ "%", op_mod };
-	{ NULL, NULL }
+	op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL }
 	};
-
-	int = 0;
+	int i = 0;
 
 	while (i < 5)
 	{
