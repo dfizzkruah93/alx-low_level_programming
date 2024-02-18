ed2d3297d0e454f2b9f39ac01965471fac2ac987
dfizzkruah93 <dfizzkruah@gmail.com>
2024-02-18 01:41:51 -0800

    error fix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -28,6 +28,11 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
+	if ((argv[2][0] == '/' || argv[2][0] == '%'))
+	{
+		printf("Error\n");
+		exit (100);
+	}
 	printf("%d\n", op_func(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
