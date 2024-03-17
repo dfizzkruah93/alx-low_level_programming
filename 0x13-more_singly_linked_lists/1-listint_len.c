11398e1489ffe5d1a0858b4b1892f7a490e70316
dfizzkruah93 <dfizzkruah@gmail.com>
2024-03-17 04:29:24 -0700

    fix error

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -4,10 +4,11 @@
 #include "lists.h"
 
 /**
- * list_len - check the code for Holberton School students.
+ * listint_len - check the code for Holberton School students.
  * @h: name of the list
  * Return: the number of nodes.
  */
+
 size_t listint_len(const listint_t *h)
 {
 	int count = 0;
