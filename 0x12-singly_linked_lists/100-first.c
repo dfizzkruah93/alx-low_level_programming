b2947e93ee900aa459904dbac7588b9737c28fd7
dfizzkruah93 <dfizzkruah@gmail.com>
2024-03-17 02:46:41 -0700

    fix error

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-/** 
+/**
  * print_message - fuction that print a message displayed
  * applying the constructor atttribute and executing before main
  */
-void myStartupFun(void) __attribute__ ((constructor));
+void print_message(void) __attribute__ ((constructor));
 
 /**
  * print_message - implementation of the function
  */
-void myStartupFun(void)
+void print_message(void)
 {
-        printf("You're beat! and yet, you must allow,\n");
-        printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
