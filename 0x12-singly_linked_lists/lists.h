58b575dce4726fef05bb5bca3bc12476446f3bc4
dfizzkruah93 <dfizzkruah@gmail.com>
2024-03-16 22:32:33 -0700

    fix error

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,19 +3,19 @@
 #include <stdio.h>
 
 /**
- * struc list_s - singly linked list
+ * struct list_s - singly linked list
  * @str: string - (malloc string)
  * @len: length of the string
  * @next: points to the next node
  *
- * @Description: singly linked list node structure
- * */
+ * Description: singly linked list node structure
+ */
 
 typedef struct list_s
 {
-        char *str;
-        unsigned int len;
-        struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 int _putchar(char c);
@@ -25,4 +25,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif 
+#endif
